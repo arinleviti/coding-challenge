e93fd0fc615e1f7fb3e39e0a6dd9bd29fc3af3cf
arinleviti <arin.leviti@gmail.com>
2025-09-11 18:12:05 +0200

    added env variable for Netlify deployment

--- src/app/lib/products.ts
+++ src/app/lib/products.ts
@@ -2,7 +2,7 @@
 import { Product } from '../page';
 
 export async function fetchProducts(): Promise<Product[]> {
- const baseUrl = process.env.BASE_URL || 'http://localhost:3000'; // server URL
+ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; // server URL
   const res = await fetch(`${baseUrl}/api/products`, { cache: 'no-store' });
 
   if (!res.ok) throw new Error('Failed to fetch products');
